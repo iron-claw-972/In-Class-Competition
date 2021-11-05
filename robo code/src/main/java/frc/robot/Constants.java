947aa909ad4e32912068c92a7740c286948a190d
OctraSource <92061729+JusticePeyton@users.noreply.github.com>
2021-11-05 16:07:56 -0700

    initial import

---
+++ robo code/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+
+//TODO 1: make the ports accurate
+public final class Constants {
+  public static final class DriveConstants {
+
+    public static final int kLeftMotor1Port = 0;
+    public static final int kRightMotor1Port = 0;
+
+    public static final int kControllerPort = 0;
+
+    public static final int kLeftJoyAxis = 0;
+    public static final int kRightJoyAxis = 0;
+  }
+}
